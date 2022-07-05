c48b61bc10701d11f4719812c67bb69503d4f735
masunulla <masunulla@gmail.com>
2022-07-05 07:19:01 +0530

    myskill

---
+++ app.js
@@ -0,0 +1,15 @@
+var http = require('http');
+
+var finalhandler = require('finalhandler');
+var serveStatic = require('serve-static');
+
+var serve = serveStatic("./");
+
+var server = http.createServer(function(req, res) {
+  var done = finalhandler(req, res);
+  serve(req, res, done);
+});
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT);
+console.log('Listening at: localhost:8080');
